d1cf014aab126ffa56a282652635557784cb8605
sgnsyn <sgnsyn45@gmail.com>
2025-10-14 18:10:10 +0300

    oops

--- lua/plugins/live-server.lua
+++ lua/plugins/live-server.lua
@@ -3,9 +3,16 @@ return {
     "barrett-ruth/live-server.nvim",
     build = "npm install -g live-server",
     config = function()
-      require("live-server").setup()
+
+      local https_conf = vim.fn.expand("~/.config/https-cert/https-conf.js")
+      local abs_path = vim.fn.fnamemodify(https_conf, ":p")
+
+      require("live-server").setup({
+        args = { "--https=" .. abs_path },
+      })
+
       vim.keymap.set("n", "<leader>ls", "<cmd>LiveServerStart<CR>", { desc = "Start Live Server" })
       vim.keymap.set("n", "<leader>lk", "<cmd>LiveServerStop<CR>", { desc = "Stop Live Server" })
-    end
+    end,
   }
 }
